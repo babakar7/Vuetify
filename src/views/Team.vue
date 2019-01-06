16f38d6d8c2a123c071557174269b33eff2af210
Babakar Bara Diop <BABS@pc300.home>
2019-01-06 22:36:43 +0000

    final

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,8 +3,30 @@
 
     <h1 class="subheading"> Team </h1>
 
-    <v-container class="my-5">
-      Content
+    <v-container class="my-5" >
+      <v-layout row wrap justify-space-between>
+
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team">
+
+          <v-card>
+
+            <v-card-text>
+              <div>
+                <p class="subheading">{{person.name}}</p>
+                <p>{{person.role}}</p>
+              </div>
+
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn depressed>
+                <v-icon>message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 
@@ -15,7 +37,9 @@
   export default {
     data(){
       return {
-
+          team:[{name:'Babakar', role:'Web Developer'},
+        {name:'Amy', role:'Lead Developer'},
+      {name:'Max', role:'Web Designer'}]
       }
     }
   }
